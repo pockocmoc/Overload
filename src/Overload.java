6a51d438d7adc2d0d3839319b93bfa2285f0ac66
pockocmocWork <pockocmoc@inbox.ru>
2022-08-05 11:02:26 +0300

    Initial commit

---
+++ src/Overload.java
@@ -0,0 +1,31 @@
+ class OverloadDemo {
+    void test() {
+        System.out.println("Параметры отсутствуют");
+    }
+     // Перегружаемый метод, проверяющий наличие
+     // двух целочисленных параметров
+
+     void test(int a, int b) {
+        System.out.println("a и b: " + a + " " + b);
+     }
+     /*
+     Перегружаемый метод, проверяющий наличие
+     параметра типа double
+      */
+    void test(double a) {
+        System.out.println("Внутреннее преобразование при вызове "
+        + " test(double) a: " + a);
+
+    }
+}
+class Overload {
+    public static void main(String[] args) {
+        OverloadDemo ob = new OverloadDemo();
+        int i = 88;
+
+        ob.test();
+        ob.test(10, 20);
+        ob.test(i);
+        ob.test(123.2);
+    }
+}
